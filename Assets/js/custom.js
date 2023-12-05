ba6090cb54446c36c0e049bf2c755e2e34e3ea97
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-12-06 00:22:47 +0600

    Complete This Page

--- Assets/js/custom.js
+++ Assets/js/custom.js
@@ -1,13 +1,18 @@
 let slideBox = document.querySelectorAll(".slide__box");
 for (let i = 0; i < slideBox.length; i++) {
   const element = slideBox[i];
-  element.addEventListener('click', function(){
-      if (element.classList.contains("activeBox")) {
-        element.classList.remove("activeBox");
-      } else {
-        element.classList.add("activeBox");
-      }
-  })
-  
+  element.addEventListener("click", function () {
+    if (element.classList.contains("activeBox")) {
+      element.classList.remove("activeBox");
+    } else {
+      element.classList.add("activeBox");
+    }
+  });
 }
-
+var thumbnails = document.querySelectorAll(".thumbnail");
+var mainImage = document.querySelector("#main-image");
+for (var i = 0; i < thumbnails.length; i++) {
+  thumbnails[i].addEventListener("click", function () {
+    mainImage.src = this.src;
+  });
+}
