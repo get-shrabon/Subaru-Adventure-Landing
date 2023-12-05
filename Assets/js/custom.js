f0d590961d07569c8feac82854132e4d45d9151a
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-12-05 20:42:24 +0600

    Complete hero Section

--- Assets/js/custom.js
+++ Assets/js/custom.js
@@ -11,19 +11,3 @@ for (let i = 0; i < slideBox.length; i++) {
   
 }
 
-// document
-//   .querySelector(".slide__box")
-//   .addEventListener("click", function (event) {
-//     event.preventDefault();
-//     var slideBox = document.querySelector(".slide__box");
-//     for (let i = 0; i < slideBox.length; i++) {
-//       const element = slideBox[i];
-//           if (element.classList.contains("activeBox")) {
-//             element.classList.remove("activeBox");
-//           } else {
-//             element.classList.add("activeBox");
-//           }
-//     }
-    
-
-//   });
